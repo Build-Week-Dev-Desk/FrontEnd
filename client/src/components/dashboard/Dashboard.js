273d692abacbd2472935ed382c1fe9fedeac941e
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-02-02 12:13:36 -0600

    made owners

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,11 +1,11 @@
-import react from 'react';
+import React from 'react';
 
 
 //components
 import CreateTicketForm from './CreateTicketForm';
 import MyTicketsStaff from './MyTicketsStaff';
 import MyTicketsStudent from './MyTicketsStudent';
-import OpenTicketList from './OpenTicketList'
+import TicketList from './TicketList'
 
 const Dashboard = props => {
 
@@ -13,17 +13,23 @@ const Dashboard = props => {
     return (
         
         <div>
+            <MyTicketsStudent/>
+            <MyTicketsStaff/>
+            <CreateTicketForm/>
+        
             {/* if user is a student and they have studentTickets then render the studentTickets */}
-            {props.user.student && (state.user.studentTickets.length > 0) && <MyTicketsStudent/>}
+            {/* {state.user.student && (state.user.studentTickets.length > 0) && <MyTicketsStudent/>} */}
     
             {/* if user is staff and they have stafftickets then render the stafftickets */}
-            {props.user.staff && (state.user.stafftickets.length > 0) && <MyTicketsStaff/>}
+            {/* {state.user.staff && (state.user.stafftickets.length > 0) && <MyTicketsStaff/>} */}
 
             {/* if user is student render createTicketForm */}
-            {props.user.student && <CreateTicketForm/>}
+            {/* {state.user.student && <CreateTicketForm/>} */}
 
             {/* render the openTicketList unconditionally */}
-            <OpenTicketList/>
+            <TicketList/>
         </div>
     )
 }
+
+export default Dashboard
