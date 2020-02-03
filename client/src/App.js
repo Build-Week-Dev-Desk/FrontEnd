c970eaf1d1beb1fb76500ae3a05d53d0749fda4f
Milo Rastgoo <hello@informagician.com>
2020-02-03 12:13:42 -0500

    LoginForm and Signup Form are created and semi-functional

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import "./App.css";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import PrivateRoute from "./tools/PrivateRoute";
+//import PrivateRoute from "./tools/PrivateRoute";
 
 //components
-import Dashboard from "./components/dashboard/Dashboard";
+//import Dashboard from "./components/dashboard/Dashboard";
 import LoginForm from "./components/login/LoginForm";
-import NavBar from "./components/NavBar";
+import SignupForm from "./components/login/SignupForm";
+//import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar />
+        {/* <NavBar /> */}
         <Route exact path="/" component={LoginForm} />
-        <PrivateRoute path="/dashboard" component={Dashboard} />{" "}
+        <Route path="/signup" component={SignupForm} />
+        {/* <PrivateRoute path="/dashboard" component={Dashboard} /> */}
       </div>
     </Router>
   );
--- client/src/components/login/SignupForm.js
+++ client/src/components/login/SignupForm.js
@@ -1,1 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SignupForm = () => {
+
+    const handleChange = () => {
+
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+    return(
+        <div>
+            <h2>Sign Up to Dev Desk</h2>
+            <label>Email Address:<input type="text" placeholder="Enter Email Address" name="email" onChange={handleChange} /></label>
+            <label>Password:<input type="password" placeholder="Enter Email Address" name="password" onChange={handleChange} /></label>
+            <label>Confirm Password:<input type="password" placeholder="Enter Email Address" name="confirmpassword" onChange={handleChange} /></label>
+            <select>
+                <option>Staff</option>
+                <option>Student</option>
+                <option>Staff &amp; Student</option>
+            </select>
+            <button onClick={handleSubmit}>Sign Up</button>
+            <Link to="/">Sign In</Link>
+        </div>
+    )
+}
+
+export default SignupForm;
 //needs to send axios call that posts the new user to the backend
