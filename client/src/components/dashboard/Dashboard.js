7d68c689baf8b1a359da31ba6377714071fa61c3
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-02-03 14:56:44 -0600

    worked on dashboard a bit

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,27 +1,95 @@
-import React from 'react';
-
+import React from "react";
+import axios from "axios";
 
 //components
-import CreateTicketForm from './CreateTicketForm';
-import TicketList from './TicketList'
+import Ticket from "./Ticket";
+
+//this needs to be taken out once there is a backend
+const state = {
+  user: {
+    id: 16,
+    student: true,
+    staff: true,
+    staffTickets: [],
+    studentTickets: [],
+    email: "myemail@email.com",
+    name: "Mr. smarty pants",
+    timeCreated: 123
+  },
+  tickets: [
+    {
+      id: 16,
+      title: "Im a big problem",
+      status: "open",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem",
+      attemptedSolutions: "I tried a whole buncha stuff man",
+      category: "html",
+      asker: "jeffrey real confused",
+      timeCreated: 213,
+      solution: null
+    },
+    {
+      id: 13,
+      title: "Im a second problem",
+      status: "closed",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem",
+      attemptedSolutions: "I tried even more things ",
+      category: "css",
+      asker: "jeffrey real confused",
+      timeCreated: 213,
+      solution: {
+        timeCreated: 92,
+        body: "I solved this problem with my brain",
+        answerer: "smarty pants guy"
+      }
+    }
+  ]
+};
 
 const Dashboard = props => {
+  const claimTicket = (e) => {
+    //needs to add itself to the user's staffTicket's array
+    //also needs to make put request and update the claimed prop of the item
+  };
+  const unclaimTicket = e => {
+    //needs to toggle the classname of the selected item to be open
+    //needs to remove itself from the user's staffTicket's array
+    //also needs to make put request and update the claimed prop of the item
+  };
+  const completeTicket = () => {
+    //needs to toggle the classname of the selected item to be completed
+    //needs to update the solution prop from null to a solution object with timeCreated,body and answerer props
+  };
 
+  const deleteTicket = () => {
+    //needs to make axios call and delete ticket from backend
+  };
 
-    return (
-        
-        <div>
-           
-        
-           
+  return (
+    <div>
+      <div className="profile-area">
+        <div>Name: {state.user.name}</div>
+        <div>Email: {state.user.email}</div>
+        {state.user.student && <div>I'm a Student</div>}
+        {state.user.staff && <div>I'm a Staff Member</div>}
+      </div>
+      <div>
+        {state.tickets.map(ticket => {
+          return (
+            <Ticket
+              claimTicket={claimTicket}
+              unclaimTicket={unclaimTicket}
+              completeTicket={completeTicket}
+              deleteTicket={deleteTicket}
+              ticket={ticket}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-            {/* if user is student render createTicketForm */}
-            {/* {state.user.student && <CreateTicketForm/>} */}
-
-            {/* render the openTicketList unconditionally */}
-            <TicketList/>
-        </div>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
--- client/src/components/dashboard/TicketList.js
+++ client/src/components/dashboard/TicketList.js
@@ -3,9 +3,54 @@ import React from "react";
 
 import Ticket from "./Ticket";
 
-function OpenTicketList(props) {
-  
-  return <div>I'm gonna be a list of all the tickets</div>;
+const state = {
+    user: {
+           id: 16,
+        student: true,
+        staff: true,
+        staffTickets: [],
+        studentTickets: [],
+        email: 'myemail@email.com',
+        name: 'Mr. User Story',
+        timeCreated: 123
+    }, 
+     tickets: [{
+        id: 16,
+        title: 'Im a big problem',
+        status: 'open',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem",
+        attemptedSolutions: 'I tried a whole buncha stuff man',
+        category: 'html',
+        asker: 'jeffrey real confused',
+        timeCreated: 213,
+        solution: null
+    }, 
+    {
+        id: 13,
+        title: 'Im a second problem',
+        status: 'closed',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem",
+        attemptedSolutions: 'I tried even more things ',
+        category: 'css',
+        asker: 'jeffrey real confused',
+        timeCreated: 213,
+        solution: {
+          timeCreated: 92,
+            body: 'I solved this problem with my brain', 
+            answerer: "smarty pants guy"
+        }
+    }]
 }
 
-export default OpenTicketList;
+function TicketList(props) {
+  
+  return (state.tickets.map(ticket => {
+    return (
+      <Ticket ticket={ticket}/>
+    )
+  }));
+}
+
+export default TicketList;
+
+
