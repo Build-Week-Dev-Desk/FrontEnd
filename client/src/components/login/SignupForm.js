5e66bd88a8da1fd934ab3eefceacaf17fec4abab
Milo Rastgoo <hello@informagician.com>
2020-02-04 15:55:02 -0500

    testing signup and login

--- client/src/components/login/SignupForm.js
+++ client/src/components/login/SignupForm.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import Axios from 'axios';
 
 const SignupForm = () => {
 
     const { register, handleSubmit } = useForm()
-    const onSubmit = data => { console.log(data) }
+    const onSubmit = data => { 
+        console.log(data)
+        // Axios.post('https://bwdevdesk.herokuapp.com/api/tickets', data)
+        // .then(res => (
+        //     console.log(res)
+
+        // ))
+        // .catch(err => console.log(err))
+     }
 
     return(
         <form onSubmit={handleSubmit(onSubmit)}>
             <h2>Sign Up to Dev Desk</h2>
+            <label>Name:<input type="text" placeholder="Enter Your Name" name="name" ref={register} /></label>
             <label>Email Address:<input type="text" placeholder="Enter Email Address" name="email" ref={register} /></label>
-            <label>Password:<input type="password" placeholder="Enter Email Address" name="password" ref={register} /></label>
-            <label>Confirm Password:<input type="password" placeholder="Enter Email Address" name="confirmpassword" ref={register} /></label>
+            <label>Password:<input type="password" placeholder="Enter Password" name="password" ref={register} /></label>
+            <label>Confirm Password:<input type="password" placeholder="Confirm Password" name="confirmpassword" ref={register} /></label>
             <label>Pick Your Group:
                 <select name="group" ref={register}>
                     <option value="staff">Staff</option>
