fec68a66259f96933492ae7d6c17d3a2818e9f35
Tim Shaker <tim@tmshkr.com>
2020-02-06 13:17:26 -0800

    update navbar font to match landing page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,56 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import { Link } from 'react-router-dom';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem,  NavLink } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-//import Axios from 'axios';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./NavBar.scss";
 
 const NavBar = props => {
-    const [ collapsed, setCollapsed ] = useState(true);
+  const [collapsed, setCollapsed] = useState(true);
 
-    const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggleNavbar = () => setCollapsed(!collapsed);
 
-    // The login to check if the user is loggedin, and if true display logout button in menu instead of login button.
-    return (
-        <div>
-            <Navbar color="solid" style={{backgroundColor: "#7ED321"}} light>
-                <NavbarBrand href={props.loggedIn ? '/dashboard' : '/'} className="home"><h2>DevDesk</h2></NavbarBrand>
-                    <NavbarToggler onClick={toggleNavbar} className="toggle"/>
-                        <Collapse isOpen={!collapsed} navbar>
-                            <Nav navbar>
-
-                                {props.loggedIn ? (
-                                    <>
-                                    <NavItem>
-                                        <NavLink href="/dashboard"><h3>Dashboard</h3></NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink href="/mytickets"><h3>My Tickets</h3></NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink href="/createticket"><h3>Create New Ticket</h3></NavLink> 
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink href="/profile"><h3>Profile</h3></NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink href="/logout" className="logout"><h3>Logout</h3></NavLink>
-                                    </NavItem>
-                                    </>
-                                ) : (
-                                    <>
-                                    <NavItem>
-                                        <NavLink href="/login" className="login"><h3>Login</h3></NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink href="/signup" className="signup"><h3>Sign Up</h3></NavLink>
-                                    </NavItem>
-                                    </>
-                                )}                              
-                            </Nav>
-                        </Collapse>
-            </Navbar>
-        </div>
-    );
-}
+  // The login to check if the user is loggedin, and if true display logout button in menu instead of login button.
+  return (
+    <div>
+      <Navbar color="solid" style={{ backgroundColor: "#7ED321" }} light>
+        <NavbarBrand
+          id="brand"
+          href={props.loggedIn ? "/dashboard" : "/"}
+          className="home"
+        >
+          DevDesk
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="toggle" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            {props.loggedIn ? (
+              <>
+                <NavItem>
+                  <NavLink href="/dashboard">
+                    <h3>Dashboard</h3>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/mytickets">
+                    <h3>My Tickets</h3>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/createticket">
+                    <h3>Create New Ticket</h3>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/profile">
+                    <h3>Profile</h3>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/logout" className="logout">
+                    <h3>Logout</h3>
+                  </NavLink>
+                </NavItem>
+              </>
+            ) : (
+              <>
+                <NavItem>
+                  <NavLink href="/login" className="login">
+                    <h3>Login</h3>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/signup" className="signup">
+                    <h3>Sign Up</h3>
+                  </NavLink>
+                </NavItem>
+              </>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default NavBar;
