e9a8708041fb37f3a6e358bf40616fe02a6242d9
Tim Shaker <tim@tmshkr.com>
2020-02-06 10:41:55 -0800

    polish buttons

--- client/src/components/dashboard/CreateTicketForm.js
+++ client/src/components/dashboard/CreateTicketForm.js
@@ -46,7 +46,7 @@ function CreateTicketForm(props) {
       status: "open"
     });
     // console.log("im in between", newTicket);
-    props.history.push('/dashboard')
+    props.history.push("/dashboard");
   };
 
   useEffect(() => {
@@ -68,68 +68,68 @@ function CreateTicketForm(props) {
     <div className="tickform">
       <h1 className="dashb">Create New Ticket</h1>
       <div className="createtick">
-      <div className="entiretick">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="ticket">
-            <label htmlFor="title">
-              <h2>Title:</h2>
-            </label>
-            <input
-              id="title"
-              type="text"
-              name="title"
-              // onChange={handleChange}
-              ref={register({ required: true })}
-            />
-            {errors.name && errors.name.type === "required" && (
-              <p>This is required</p>
-            )}
-          </div>
-          <div className="ticket">
-            <label htmlFor="description">
-              <h3>Description:</h3>
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              // onChange={handleChange}
-              ref={register({ required: true })}
-            />
-            {errors.description && errors.description.type === "required" && (
-              <p>This is required</p>
-            )}
-          </div>
-          <div className="ticket">
-            <label htmlFor="category">
-              <h3>Category:</h3>
-            </label>
-            <select name="category" ref={register({ required: true })}>
-              <option value="">Select...</option>
-              <option value="html">Html</option>
-              <option value="css">CSS</option>
-              <option value="javascript">Javascript</option>
-              <option value="python">Python</option>
-            </select>
-            {errors.category && errors.category.type === "required" && (
-              <p>This is required</p>
-            )}
-          </div>
-          <div className="ticket">
-            <label htmlFor="attemptedSolutions">
-              <h3>What have you tried:</h3>
-            </label>
-            <textarea
-              id="attemptedSolutions"
-              name="attemptedSolutions"
-              // onChange={handleChange}
-              ref={register({ required: true })}
-            />
-            {errors.solutions && errors.solutions.type === "required" && (
-              <p>This is required</p>
-            )}
-          </div>
-          <input type="submit" className="submitbtn" />
-        </form>
+        <div className="entiretick">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="ticket">
+              <label htmlFor="title">
+                <h2>Title:</h2>
+              </label>
+              <input
+                id="title"
+                type="text"
+                name="title"
+                // onChange={handleChange}
+                ref={register({ required: true })}
+              />
+              {errors.name && errors.name.type === "required" && (
+                <p>This is required</p>
+              )}
+            </div>
+            <div className="ticket">
+              <label htmlFor="description">
+                <h3>Description:</h3>
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                // onChange={handleChange}
+                ref={register({ required: true })}
+              />
+              {errors.description && errors.description.type === "required" && (
+                <p>This is required</p>
+              )}
+            </div>
+            <div className="ticket">
+              <label htmlFor="category">
+                <h3>Category:</h3>
+              </label>
+              <select name="category" ref={register({ required: true })}>
+                <option value="">Select...</option>
+                <option value="html">Html</option>
+                <option value="css">CSS</option>
+                <option value="javascript">Javascript</option>
+                <option value="python">Python</option>
+              </select>
+              {errors.category && errors.category.type === "required" && (
+                <p>This is required</p>
+              )}
+            </div>
+            <div className="ticket">
+              <label htmlFor="attemptedSolutions">
+                <h3>What have you tried:</h3>
+              </label>
+              <textarea
+                id="attemptedSolutions"
+                name="attemptedSolutions"
+                // onChange={handleChange}
+                ref={register({ required: true })}
+              />
+              {errors.solutions && errors.solutions.type === "required" && (
+                <p>This is required</p>
+              )}
+            </div>
+            <button className="submitbtn green">Submit</button>
+          </form>
         </div>
       </div>
     </div>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from "../../tools/axiosWithAuth";
-
+import { Link } from "react-router-dom";
 
 //components
 import Ticket from "./Ticket";
@@ -11,22 +11,27 @@ const Dashboard = props => {
     axiosWithAuth()
       .get("https://bwdevdesk.herokuapp.com/api/tickets")
       .then(res => {
-        console.log('im an axios call that actually happens and works');
+        console.log("im an axios call that actually happens and works");
         setTickets(res.data);
       })
       .catch(err => console.log(err));
   }, [setTickets]);
-  
+
   return (
     <div>
       <h1 className="dashb">Dashboard</h1>
       <div className="subnav">
-        <a className="navcreate" href="/createticket">Create New Ticket</a>
+        <button
+          className="navcreate green"
+          onClick={() => props.history.push("/createticket")}
+        >
+          Create New Ticket
+        </button>
       </div>
       <div>
         {tickets.map(ticket => {
-          console.log(ticket)
-          console.log('dashboard user is logged in as', props.userType)
+          console.log(ticket);
+          console.log("dashboard user is logged in as", props.userType);
           return (
             <>
               <Ticket
@@ -38,7 +43,6 @@ const Dashboard = props => {
             </>
           );
         })}
-        
       </div>
     </div>
   );
